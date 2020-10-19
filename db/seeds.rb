f253b0ee8c9ec4b005e86f470e1f56a749c574c6
Abel <abel.lavieri@gmail.com>
2020-10-19 16:08:08 -0400

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do |e|
-  User.create(email: "email#{e}@email.com", name: "name #{e}", password: "123456")
+  User.create(email: "email#{e+1}@email.com", name: "name #{e+1}", password: "123456")
 end
 
 5.times do |e|
-  Friendship.create(requester_id: 1, reciever_id: e+2, status: e.even?)
+  Friendship.create(requester_id: 1, receiver_id: e+2, status: e.even?)
+end
+
+5.times do |e|
+  Post.create(user_id: e+1, content: "Content for post #{e+1}")
+end
+
+5.times do |e|
+  Comment.create(user_id: e+1, post_id: e+1, content: "Content for comment #{e+1}")
+end
+
+5.times do |e|
+  Like.create(user_id: e+1, post_id: e+1)
 end
